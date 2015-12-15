5b861f454f0d69016a55f9aca9d2c860e611adae
zberman <zberman@outlook.com>
2015-12-14 19:16:42 -0500

    project 4 folder

---
+++ project4/routes/admin-routes.js
@@ -0,0 +1,273 @@
+var express = require('express');
+
+// This gives us access to the user "model".
+var model = require('../lib/user');
+
+// A list of users who are online:
+var online = require('../lib/online').online;
+
+// This creates an express "router" that allows us to separate
+// particular routes from the main application.
+var router = express.Router();
+
+// router.get('/list', (req, res) => {
+//   // TODO: Add the admin list route.
+//   // The admin list route lists the current users in the system and
+//   // provides a form to add a new user. You must make sure you do
+//   // the following in this route:
+
+//      var user = req.session.user;
+//      if(!user)
+//      {
+//         req.flash('login', 'User object does not exist!');
+//         res.redirect('/user/login');
+//      } 
+
+//      else if(user && !online[user.name])
+//      {
+//         req.flash('login', 'Login expired!');
+//         res.redirect('/user/login');
+//      }
+
+//      else if(user.admin === false)
+//      {
+//         req.flash('main', 'You need admin credentials to access this route!');
+//         res.redirect('/user/main'); 
+//      }
+
+//      else
+//      {
+//         var list = model.list(function(error, allUsers){
+
+//           if(error)
+//           {
+//               req.flash('login', 'User already exists in database!');
+//               res.redirect('/user/login');
+//           }
+
+//           else
+//           {
+//               var message = req.flash('admin');
+//               res.render('admin', {message: message, users: allUsers});
+//           }
+//         });
+//      }
+
+//   //   (1) Grab the user session object.
+//   //   (2) Test that the user session object exists. If not, a redirect
+//   //       back to the login view is necessary with a proper flash message.
+//   //   (3) Test if the user session exists and they are not online. If
+//   //       the user session exists and they are not online it means the
+//   //       server has been restarted and their session has expired. If
+//   //       this is the case you will need to redirect back to login with
+//   //       a proper flash message (e.g., login expired).
+//   //   (4) Test is the user is an admin. If they are not you need to
+//   //       redirect back to main with a proper flash message - indicate
+//   //       that the user needs admin credentials to access this route.
+//   //   (5) If the user is logged in, is online, and is an admin then
+//   //       you want to retrieve the list of users from the `lib/user.js`
+//   //       library and render the `user-list` view. The `user-list` view
+//   //       expects an array of users and a message. You should grab the
+//   //       flash message - if one exists, and pass it to the view template.
+//   //       A flash message will exist if the user tried to create a new
+//   //       user that already exists in our mock database.
+//   //
+//   //  You will be graded on each of the above items.
+
+//   // Replace below with your own implementation.
+// });
+
+// router.post('/user', (req, res) => {
+//   // TODO: Implement the /user route.
+//   // This route is similar to the /user/auth route in that it does not
+//   // have an associated view. Rather, its job is to add a new user and
+//   // redirect to /admin/list. Its job is to add a new user if the user
+//   // does not already exist in our model. You must make sure you do
+//   // the following in this route:
+
+//      var user = req.session.user;
+//      if(!user)
+//      {
+//         req.flash('login', 'User object does not exist!');
+//         res.redirect('/user/login');
+//      } 
+
+//      else if(user && !online[user.name])
+//      {
+//         req.flash('login', 'Login expired!');
+//         res.redirect('/user/login');
+//      }
+
+//      else if(user.admin === false)
+//      {
+//         req.flash('main', 'You need admin credentials to access this route!');
+//         res.redirect('/user/main'); 
+//      }
+
+//      else
+//      {
+//         var name  = req.body.name;
+//         var pass  = req.body.pass;
+//         var admin = req.body.admin;
+//         // console.log('name ' + name);
+//         // console.log('pass ' + pass);
+//         // console.log(!name);
+//         if(!name || !pass)
+//         {
+//             req.flash('user-list', 'Missing one or more required fields.');
+//             res.redirect('/admin/list');
+//         }
+
+//         else 
+//         {
+//           var newUser = {name, pass, admin};
+//             model.add(newUser, function(error, newUser){
+//               if(error)
+//               {
+//                   req.flash('user-list', error);
+//                   res.redirect('/admin/list');
+//               }
+
+//               else
+//               {
+//                   req.flash('user-list', 'User has been added!');
+//                   res.redirect('/admin/list');
+//               }
+//             });
+//         }
+//      }
+//   //   (1) Grab the user session object.
+//   //   (2) Test that the user session object exists. If not, a redirect
+//   //       back to the login view is necessary with a proper flash message.
+//   //   (3) Test if the user session exists and they are not online. If
+//   //       the user session exists and they are not online it means the
+//   //       server has been restarted and their session has expired. If
+//   //       this is the case you will need to redirect back to login with
+//   //       a proper flash message (e.g., login expired).
+//   //   (4) Test is the user is an admin. If they are not you need to
+//   //       redirect back to main with a proper flash message - indicate
+//   //       that the user needs admin credentials to access this route.
+//   //   (5) If the user is logged in, they are online, and they are an
+//   //       admin then you need to grab the form variables from the
+//   //       `req.body` object. Test to make sure they all exist. If they
+//   //       do not then you need to redirect back to the `/list` route
+//   //       defined above with a proper flash message.
+//   //   (6) If you have received the proper form variables then you must
+//   //       create a new user using the `model.add` function. If an error
+//   //       message is returned in the callback you should flash that message
+//   //       to the `list` route above passing it the error message returned
+//   //       from the `model.add` function and redirect to `list`.
+//   //       Otherwise, you should flash to `list` that the user has
+//   //       been added and redirect back to the `list` route.
+//   //
+//   //  You will be graded on each of the above items.
+
+//   // Replace below with your implementation.
+  
+// });
+
+router.get('/', (req, res) => {
+
+  var user = req.session.user;
+     if(!user)
+     {
+        req.flash('login', 'User object does not exist!');
+        res.redirect('/user/login');
+     } 
+
+     else if(user && !online[user.name])
+     {
+        req.flash('login', 'Login expired!');
+        res.redirect('/user/login');
+     }
+
+     else if(user.admin === false)
+     {
+        req.flash('main', 'You need admin credentials to access this route!');
+        res.redirect('/user/main'); 
+     }
+
+     else
+     {
+        var list = model.list(function(error, allUsers){
+
+          if(error)
+          {
+              req.flash('login', 'User already exists in database!');
+              res.redirect('/user/login');
+          }
+
+          else
+          {
+              var message = req.flash('admin');
+              res.render('admin', {message: message, 
+                                   users  : allUsers,
+                                   button : "Logout",
+                                   buttonwidth: 40,
+                                   message:message,
+                                   link   : "/user/logout"});
+          }
+        });
+     }
+});
+
+router.post('/change', (req, res) => {
+  var user = req.session.user;
+  var name = req.body.name;
+
+  if (user && online[name]) {
+    delete online[name];
+  }
+
+  if (!user || !online[user.name]) {
+    res.redirect('/user/login');
+  } 
+  else if (user.admin) {
+    model.change(name, function(error, aName) {
+      if (error) {
+        req.flash('admin', error);
+        res.redirect('/admin');
+      } 
+      else {
+        req.flash('admin', 'Admin status changed.');
+        res.redirect('/admin');
+      }
+    });
+  }
+
+});
+// router.post('/deleting', (req, res) => {
+//   // Grab the user session if logged in.
+//   var user = req.session.user;
+//   var name = req.body.name;
+
+//   if (user && online[name]) {
+//     delete online[name];
+//   }
+
+//   if(!user || !online[user.name])
+//   {
+//     res.redirect('/user/login');
+//   }
+
+//   else
+//   {
+//     model.deleting(name, function(error, aName) {
+//       if (error) {
+//         // Pass a message to login:
+//         req.flash('/', error);
+//         res.redirect('/admin');
+//       }
+//       else {
+//         console.log('end');
+//         // Pass a message to main:
+//         req.flash('/', 'Delete successful');
+//         res.redirect('/admin');
+//       }
+//     });
+//   }
+  
+// });
+
+
+module.exports = router;
---
+++ project4/routes/user-routes.js
@@ -0,0 +1,252 @@
+var express = require('express');
+
+// This gives us access to the user "model".
+var model = require('../lib/user');
+
+// This creates an express "router" that allows us to separate
+// particular routes from the main application.
+var router = express.Router();
+
+// A list of users who are online:
+var online = require('../lib/online').online;
+
+// Provides a login view
+router.get('/login', (req, res) => {
+  // Grab the session if the user is logged in.
+  var user = req.session.user;
+
+  // Redirect to main if session and user is online:
+  if (user && online[user.name]) {
+    res.redirect('/user/main');
+  }
+  else {
+    // Grab any messages being sent to us from redirect:
+    var message = req.flash('login') || '';
+    res.render('login', { button  : 'Register', 
+                          buttonwidth : 40,
+                          message:message,
+                          link    : "/user/register" });
+  }
+});
+
+// Performs **basic** user authentication.
+router.post('/auth', (req, res) => {
+  // Grab the session if the user is logged in.
+  var user = req.session.user;
+
+  // Redirect to main if session and user is online:
+  if (user && online[user]) {
+    res.redirect('/user/main');
+  }
+  else {
+    // Pull the values from the form:
+    var name = req.body.name;
+    var pass = req.body.pass;
+
+    if (!name || !pass) {
+      req.flash('login', 'Please do not leave any fields blank.');
+      res.redirect('/user/login');
+    }
+    else {
+      model.lookup(name, pass, function(error, user) {
+        if (error) {
+          // Pass a message to login:
+          req.flash('login', error);
+          res.redirect('/user/login');
+        }
+        else {
+          // add the user to the map of online users:
+          online[user.name] = user;
+
+          // create a session variable to represent stateful connection
+          req.session.user = user;
+
+          // Pass a message to main:
+          req.flash('main', 'Authentication successful!');
+          res.redirect('/user/main');
+        }
+      });
+    }
+  }
+});
+
+// Performs logout functionality - it does nothing!
+router.get('/logout', function(req, res) {
+  // Grab the user session if logged in.
+  var user = req.session.user;
+
+  // If the client has a session, but is not online it
+  // could mean that the server restarted, so we require
+  // a subsequent login.
+  if (user && !online[user.name]) {
+    delete req.session.user;
+  }
+  // Otherwise, we delete both.
+  else if (user) {
+    delete online[user.name];
+    delete req.session.user;
+  }
+
+  // Redirect to login regardless.
+  res.redirect('/user/login');
+});
+
+// Renders the main user view.
+router.get('/main', function(req, res) {
+  // Grab the user session if it exists:
+  var user = req.session.user;
+
+  // If no session, redirect to login.
+  if (!user) {
+    req.flash('login', 'Not logged in');
+    res.redirect('/user/login');
+  }
+  else if (user && !online[user.name]) {
+    req.flash('login', 'Login Expired');
+    delete req.session.user;
+    res.redirect('/user/login');
+  }
+  
+  else if (user.admin) {
+    var message = req.flash('main') || 'Login Successful';
+    res.render('newsfeed', {username : user.name,
+                          button   : 'Logout',
+                          buttonwidth : 20,
+                          link     :'/user/logout',
+                          message: message,
+                          adminbutton: 
+    "<a href = '/admin'><button class='sbtn'>Users</button></a>"
+                          });
+  }
+  else {
+    // capture the user object or create a default.
+    var message = req.flash('main') || 'Login Successful';
+    res.render('newsfeed', {username : user.name,
+                          button   : 'Logout',
+                          buttonwidth : 40,
+                          message: message,
+                          link     :'/user/logout'});
+  }
+});
+
+router.get('/branch', function(req,res) {
+  var user = req.session.user;
+
+  if(!user)
+  {
+    req.flash('login', 'Not logged in');
+    res.redirect('/user/login');
+  }
+  
+  else if(user && !online[user.name])
+  {
+    req.flash('login', 'Login Expired');
+    delete req.session.user;
+    res.redirect('/user/login');
+  }
+
+  else if (user.admin) {
+    var message = req.flash('main') || 'Login Successful';
+    res.render('branch', {username : user.name,
+                          button   : 'Logout',
+                          buttonwidth : 20,
+                          link     :'/user/logout',
+                          message:message,
+                          adminbutton: 
+    "<a href = '/admin'><button class='sbtn'>Users</button></a>"
+                          });
+  }
+  else {
+    // capture the user object or create a default.
+    var message = req.flash('main') || 'Login Successful';
+    res.render('branch', {username : user.name,
+                          button   : 'Logout',
+                          buttonwidth : 40,
+                          message:message,
+                          link     :'/user/logout'});
+  }
+
+});
+
+
+router.get('/register', (req, res) => {
+     var user = req.session.user;
+    
+     if(user && online[user.name])
+     {
+        req.flash('branch', 'Login expired!');
+        res.redirect('/user/main');
+     }
+
+     else
+     {
+        var message = req.flash('register') || '';
+        res.render('register', {button : "Login",
+                                buttonwidth : 40,
+                                message:message,
+                                link   : "/user/login"});
+        // var name  = req.body.name;
+        // var pass  = req.body.pass;
+        // var email = req.body.email;
+        // // console.log('name ' + name);
+        // // console.log('pass ' + pass);
+        // // console.log(!name);
+        // if(name && pass && email)
+        // {
+        //     req.flash('register', 'Missing one or more required fields.');
+        //     res.redirect('/user/register');
+        // }
+
+        // else 
+        // {
+        //   var newUser = {name, pass, admin};
+        //     model.add(newUser, function(error, newUser){
+        //       if(error)
+        //       {
+        //           req.flash('register', error);
+        //           res.render('/user/register');
+        //       }
+
+        //       else
+        //       {
+        //           req.flash('login', 'User has been added!');
+        //           res.redirect('/user/login');
+        //       }
+        //     });
+        // }
+     }
+  
+});
+
+router.post('/add', (req, res) => {
+ 
+  // Pull the values from the form:
+  var name  = req.body.name;
+  var pass  = req.body.pass;
+  var admin = false;
+  var email = req.body.email;
+
+  if (!name || !pass || !email) {
+    req.flash('register', 'Error: Please make sure to provide all proper credentials.');
+    res.redirect('/user/register');
+  }
+  else {
+    var newUser = {name, pass, admin, email};
+    model.add(newUser, function(error, user) {
+      if (error) {
+        // Pass a message to login:
+        req.flash('register', error);
+        res.redirect('/user/register');
+      }
+      else {
+        
+        // Pass a message to main:
+        req.flash('login', 'Registration complete. You may now log in.');
+        res.redirect('/user/login');
+      }
+    });
+  }
+  
+});
+
+module.exports = router;
